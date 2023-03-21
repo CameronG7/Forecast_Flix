bb0cf4d4d62ea4499d6e8585433388fdf45e9b19
CameronG7 <122698132+CameronG7@users.noreply.github.com>
2023-03-20 18:35:45 -0700

    added config

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
+const WEATHERKEY = config.WEATHERKEY
+const MOVIEKEY = config.MOVEIEKEY
+
 function getApi() {
   // fetch request gets a list of all the repos for the node.js organization
-  var requestUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=070b18d2b6b4f6fd43d39385a2c35cb9&language=en-US';
-  var requestUrl2= 'https://api.openweathermap.org/data/2.5/weather?q=Renton&appid=0d556524fc925415c387efcd51d5b68a'; //liza this is the api key for the weather api
+  var requestUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${MOVIEKEY}&language=en-US`;
+  var requestUrl2= `https://api.openweathermap.org/data/2.5/weather?q=   add City name   &appid=${WEATHERKEY}`; //liza this is the api key for the weather api
   
 // fetch to make an HTTP API request using JS 
 // fetch takes 2 param 
