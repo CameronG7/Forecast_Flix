aa5e7348f4fe8891c6299fb162e94793a2f733ba
LizaSmirnov <122588135+LizaSmirnov@users.noreply.github.com>
2023-03-20 21:34:15 -0700

    weather minus icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,37 @@
 const WEATHERKEY = config.WEATHERKEY
 const MOVIEKEY = config.MOVEIEKEY
-
-function getApi() {
-  // fetch request gets a list of all the repos for the node.js organization
-  var requestUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${MOVIEKEY}&language=en-US`;
-  var requestUrl2= `https://api.openweathermap.org/data/2.5/weather?q=   add City name   &appid=${WEATHERKEY}`; //liza this is the api key for the weather api
-  
-// fetch to make an HTTP API request using JS 
-// fetch takes 2 param 
-// 1 is the url we want to make the request to
-// 2 is a configuration object 
-// this code is "asynchronous" meaning its going to take some time to complete
-
-  fetch(requestUrl)
+//placeholder for searchbtn dnu
+let searchBtn = $('#searchButton');
+searchBtn.on('click', function (event) {
+  event.preventDefault();//need this
+  fetchWeather();//need this
+})
+function fetchWeather(){
+  var key = '0d556524fc925415c387efcd51d5b68a';//cameron it didnt workvar searchedName = data.city.name;
+  var newCity = $('#cityInput').val();
+  //this will be var location
+  var requestUrl2= `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&appid=`+ key + '&units=imperial';
+  console.log(newCity); // sanity check
+  fetch(requestUrl2)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data)
-      //Loop over the data to generate a table, each table row will have a link to the repo url
-     
-    });
-}
+      cityName = data.name
+      let nameChange=$('.card-title')
+      nameChange.text(cityName)
+      des = data.weather[0].description
+      console.log(des)
+      let description = $('#weather-description')
+       description.text(des)
+      cityTemp = data.main.temp
+      let temp = $('#temperature')
+      temp.text(cityTemp)
+      cityIcon = data.weather[0].icon
+      let iconImg = $('.card')
+      iconImg.attr = ('img src','https://openweathermap.org/img/wn/'+cityIcon+'.png')
+      console.log(iconImg);
+    
+    })
+  };
